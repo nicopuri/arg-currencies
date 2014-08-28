ebba61fb0defa975863be4006fe6123b24e63c70
Nicolas Purita <nicospuri@gmail.com>
2014-08-28 13:21:23 -0300

    Initial commit

---
+++ arg-dolarblue-btc.gemspec
@@ -0,0 +1,20 @@
+Gem::Specification.new do |s|
+  s.name          = 'arg-dolarblue-btc'
+  s.version       = '0.1.1'
+  s.date          = '2014-08-28'
+  s.summary       = "Dolar Blue and BTC price"
+  s.description   = "A simple gem that show you the Dolar Blue and BTC price. The BTC price is from Bitstamp."
+  s.authors       = ["Nicolas Purita"]
+  s.email         = 'nicospuri@gmail.com'
+  s.files         = ["lib/arg-dolarblue-btc.rb", "bin/arg-blues"]
+  s.homepage      = ''
+  s.license       = 'MIT'
+  s.bindir        = 'bin'
+  s.executables   = ["arg-blues"]
+  s.has_rdoc      = false
+
+  s.add_runtime_dependency 'colorize', '~> 0.7'
+  s.add_runtime_dependency 'httparty', '~> 0.13'
+
+  s.post_install_message = "To use execute 'arg-blues'"
+end
---
+++ lib/arg-dolarblue-btc.rb
@@ -0,0 +1,33 @@
+require 'httparty'
+require 'colorize'
+
+class ArgCurrency
+
+	def initialize(argv=[""])
+		@urlDolar = 'http://ws.geeklab.com.ar/dolar/get-dolar-json.php'
+		@urlBitstamp = 'https://www.bitstamp.net/api/ticker/'
+	end
+
+	def get_currencies
+		response = HTTParty.get(@urlDolar)
+
+		json = JSON.parse(response.body)
+
+		puts '================= DOLAR ================'
+		puts "Dolar Blue:".colorize(:blue).bold.underline + " $#{json['blue']}"
+		puts "Dolar Oficial:".colorize(:green).bold.underline + " $#{json['libre']}"
+
+		#BTC
+		response = HTTParty.get(@urlBitstamp)
+		json = JSON.parse(response.body)
+
+		puts '================= BTC =================='
+		puts 'Last:'.colorize(:light_black).underline + " $#{json['last']}".bold
+		puts 'High:'.colorize(:light_black).underline + " $#{json['high']}"
+		puts 'Low:'.colorize(:light_black).underline + " $#{json['low']}"
+		puts 'Bid:'.colorize(:light_black).underline + " $#{json['bid']}"
+		puts 'Ask:'.colorize(:light_black).underline + " $#{json['ask']}"
+		puts 'Volume:'.colorize(:light_black).underline + " #{json['volume']}"
+		puts '========================================'
+	end
+end
